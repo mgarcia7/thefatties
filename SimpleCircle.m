0d1ef1a5b290d9e76b8373a8f743fe3437c764dd
Margaret <mbolick@bu.edu>
2016-10-16 22:40:07 -0400

    Updated Sample Image

--- SimpleCircle.m
+++ SimpleCircle.m
@@ -1,9 +1,17 @@
 close all
 figure
 
- X = rand(5,1);
- Y = rand(5,1);
- centers = [X Y];
- radii = 0.1*rand(5,1);
+ %X = rand(5,1);
+ %Y = rand(5,1);
+ %centers = [X Y];
+ %radii = 0.1*rand(5,1);
     
- viscircles(centers,radii,'Color','black');
+ %viscircles(centers,radii);
+
+ 
+ for i = 1:10
+     pos = [(i+2), (i+3), 0.5, 0.5]; 
+     rectangle('Position',pos,'Curvature',[1 1],'FaceColor','w')
+ end
+ axis equal
+ set(gca,'Color',[0 0 0]);
