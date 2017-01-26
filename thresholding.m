563635a3fe5c648ca72fa650b8a9ef0382639d6e
Margaret <mbolick@bu.edu>
2017-01-26 00:38:49 -0500

    Updated Image Uploading/Text File Output
    Considering separating images in folder by sample, but unsure.

--- thresholding.m
+++ thresholding.m
@@ -4,72 +4,99 @@ close all
 %Article Below
 %https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4442582/#SD1
 % Reads in sample, if it has three channels, takes only the first channel
-fname = 'R1D2S1d00I1wi.tif';
-im = imread(fname);
-if size(im,3) == 3
-    im = im(:, :, 1);
+%fname = 'R1D2S1d00I1wi.tif';
+basePath = 'Data/Round1/Design2/'; %Basepath of where the data is
+allPaths = dir(basePath);  %Gets all content from directory
+subFolders = [allPaths(:).isdir]; %Gets other subfolders
+foldersNames = {allPaths(subFolders).name}'; %Sort subfolder names
+foldersNames(ismember(foldersNames,{'.','..'})) = []; %Deletes the default folders
+for i=1:length(foldersNames), %Loop through subfolders
+    tmp = foldersNames{i};  %Folder Names
+    currentPath =strcat([basePath tmp]); %Path for next subfolder
+    cd(currentPath);   %Transfers to new directory
+    files = dir('*.tif'); %List of images
+    for j=1:length(files), %Loops through images
+        im = imread(files(j).name); %Reads in images
+        disp(files(j).name);
+        if size(im,3) == 3
+            im = im(:, :, 1);
+        end
+        %%
+        % Converts image to a value btw 0 to 1
+        %im_gray = double(im)/255;
+        
+        % Proportionally scales the image so that 0 = min val and 1 = max val
+        im_gray = imadjust(im);
+        %imshow(im_gray)
+        
+        %%
+        
+        % Creating a binary image using threshold found above
+        threshold = graythresh(im_gray);
+        bin_image = im2bw(im_gray,threshold*1.57);
+        
+        % Fill the holes in da image
+        Filled_Image=imfill(bin_image,'holes');
+        
+        % Filtering
+        % TODO: decide which filter and disk size, maybe try to find it
+        % programmatically
+        bin_image = medfilt2(Filled_Image,[8 8]);
+        
+        %figure
+        %imshow(bin_image)
+        
+        %%
+        % Labels the images
+        [labeled_im,num_of_objects]=bwlabel(bin_image,8);
+        
+        % Gets the blob properties
+        blobMeasurements = regionprops(labeled_im,'all');
+        allBlobAreas = [blobMeasurements.Area];
+        allBlobPerimeters = [blobMeasurements.Perimeter];
+        allBlobExtents = [blobMeasurements.Extent];
+        
+        allBlobCircularities = (4*3.14.*allBlobAreas)./(allBlobPerimeters.^2); % Isoperimetric inequality
+        
+        % Logical vector that has 1 or 0 depending on whether it meets the
+        % threshold
+        allowableArea = (allBlobAreas > 6);
+        allowableExtent = (allBlobExtents >= 0.50);
+        allowableCircularity = (allBlobCircularities >= 0.8);
+        
+        % Extracts the circular ones!!
+        % TODO: Improve on this? Maybe use circularity instead of extent? Maybe
+        % eccentricity??
+        BWcircles = ismember(labeled_im, find(allowableArea & (allowableCircularity | allowableExtent)));
+        
+        %figure(2)
+        % Plots the blobs on top of the original image
+        %imshow(im_gray)
+        hold on
+        numberOfBlobs = size(find(allowableExtent),2);
+        
+        boundaries = bwboundaries(BWcircles);
+        numberOfBoundaries = size(boundaries,1);
+        for k = 1 : numberOfBoundaries
+            thisBoundary = boundaries{k};
+            %plot(thisBoundary(:,2), thisBoundary(:,1), 'g', 'LineWidth', 2);
+        end
+        hold off
+    end
+    [allBlobAreasMicro] = (allBlobAreas.*6.7)./10;
+    avgArea = mean(allBlobAreasMicro);
+    textFileName = [tmp '.txt'];
+    fileID = fopen(textFileName, 'w');
+    fprintf(fileID, '%s Measurements\n', textFileName);
+    fprintf(fileID, '%3.2f \n', allBlobAreasMicro);
+    fprintf(fileID, 'Average Area: %3.2f', avgArea);
+    fclose(fileID);
+    cd ..;
+    cd ..;
+    cd ..;
+    cd ..;
+    
 end
-%%
-% Converts image to a value btw 0 to 1
-%im_gray = double(im)/255; 
-
-% Proportionally scales the image so that 0 = min val and 1 = max val
-im_gray = imadjust(im); 
-imshow(im_gray)
-
-%%
-
-% Creating a binary image using threshold found above
-threshold = graythresh(im_gray);
-bin_image = im2bw(im_gray,threshold*1.57);
-
-% Fill the holes in da image
-Filled_Image=imfill(bin_image,'holes');
-
-% Filtering
-% TODO: decide which filter and disk size, maybe try to find it
-% programmatically
-bin_image = medfilt2(Filled_Image,[8 8]);
-
-figure
-imshow(bin_image)
-
-%%
-% Labels the images
-[labeled_im,num_of_objects]=bwlabel(bin_image,8);
-
-% Gets the blob properties
-blobMeasurements = regionprops(labeled_im,'all');
-allBlobAreas = [blobMeasurements.Area];
-allBlobPerimeters = [blobMeasurements.Perimeter];
-allBlobExtents = [blobMeasurements.Extent];
-
-allBlobCircularities = (4*3.14.*allBlobAreas)./(allBlobPerimeters.^2); % Isoperimetric inequality
-
-% Logical vector that has 1 or 0 depending on whether it meets the
-% threshold
-allowableArea = (allBlobAreas > 6);
-allowableExtent = (allBlobExtents >= 0.50);
-allowableCircularity = (allBlobCircularities >= 0.8); 
-
-% Extracts the circular ones!!
-% TODO: Improve on this? Maybe use circularity instead of extent? Maybe
-% eccentricity??
-BWcircles = ismember(labeled_im, find(allowableArea & (allowableCircularity | allowableExtent)));
-
-figure(2)
-% Plots the blobs on top of the original image
-imshow(im_gray)
-hold on
-numberOfBlobs = size(find(allowableExtent),2);
-
-boundaries = bwboundaries(BWcircles);
-numberOfBoundaries = size(boundaries,1);
-for k = 1 : numberOfBoundaries
-	thisBoundary = boundaries{k};
-	plot(thisBoundary(:,2), thisBoundary(:,1), 'g', 'LineWidth', 2);
-end
-hold off
 
 %% Get stats
-createconfusionmat(fname,BWcircles);
+%createconfusionmat(fname,BWcircles);
